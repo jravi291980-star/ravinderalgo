39c2011e474f4ea40eef227094c3c42ccb0600dc
jravi291980-star <jravi291980@gmail.com>
2025-11-28 13:17:53 +0000

    done

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.dashboard_view, name='dashboard'),
     # New dedicated path to handle the redirect from Dhan's authentication flow
-    path('dhan-callback/', views.dhan_callback_view, name='dhan_callback'), 
 ]
